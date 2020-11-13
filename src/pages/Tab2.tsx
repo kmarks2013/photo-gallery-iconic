3bc2fba31a0e807d14fd4aa34064923a3888c072
kmarks2013 <kmarks2013@gmail.com>
2020-11-13 17:24:55 -0500

    adds camera trash and close icon imports

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons'
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
