6b1524538e59829fec3a5f4a74be0471097d6848
kmarks2013 <kmarks2013@gmail.com>
2020-11-13 18:23:30 -0500

    adds eport to usePhotoGallery ;

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -4,3 +4,7 @@ import { useFilesystem, base64FromPath } from '@ionic/react-hooks/filesystem'
 import { useStorage } from '@ionic/react-hooks/storage'
 import { isPlatform } from '@ionic/react'
 import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core"
+
+export const usePhotoGallery = () => {
+
+}
