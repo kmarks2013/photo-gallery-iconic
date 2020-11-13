b39f1744c367c2a572b83c0d6b122f2f32822bba
kmarks2013 <kmarks2013@gmail.com>
2020-11-13 18:01:33 -0500

    replaces Tab2 with Photos

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ const App: React.FC = () => (
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={images} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>Photos</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
